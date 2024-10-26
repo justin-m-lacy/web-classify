8f0e70f52cede9a2168974aa0817ae1ff760df00
Justin Lacy <justin.m.lacy@gmail.com>
2024-10-26 10:37:27 -0700

    remove unused tailwind css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import Forms from '@tailwindcss/forms';
-import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette';
 import plugin from 'tailwindcss/plugin';
 
 /** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
@@ -8,16 +7,6 @@ export default {
   content: ['./src/**/*.{vue,js,ts,jsx,tsx,html}', 'index.html'],
   theme: {
 
-
-    fontFamily: {
-      discog: ['discog'],
-      saiba: ['saiba'],
-      tekex: ['tekex'],
-      audiowide: ['audiowide'],
-      greenwash: ['greenwash'],
-      miracode: ['miracode']
-
-    },
     extend: {
 
       gridTemplateColumns: {
@@ -27,15 +16,6 @@ export default {
         'grid-template': 'repeat(auto-fill, minmax(1fr, 1fr))'
       },
       keyframes: {
-
-        throb: {
-          '0%,100%': {
-            color: 'var(--throb-on-color)'
-          },
-          '50%': {
-            color: 'var(--throb-off-color)'
-          }
-        },
         blink: {
           '0%,100%': {
             opacity: 1
@@ -80,60 +60,7 @@ export default {
       blur: {
         line: '1px'
       },
-      colors: {
-        'matrix': {
-          600: 'rgb(22 163 41 / <alpha-value>)'
-        },
-        'aqua': {
-          200: "rgb(1 220 231 / <alpha-value>)",
-          300: "rgb(5 200 210 / <alpha-value>)",
-          400: "rgb(51 212 236 / <alpha-value>)",
-          500: "rgb(5 176 210 / <alpha-value>)",
 
-        },
-
-        "dark-grey": 'rgb(37 40 51 / <alpha-value>)',
-        'laser-red': 'rgb(243 22 22 / <alpha-value>)',
-        'matte': {
-          100: "rgb(47 48 52 / <alpha-value>)",
-          200: "rgb(32 33 36 / <alpha-value>)"
-        },
-        "bluez": {
-          "100": "rgb(220 244 248 / <alpha-value>)",
-          "200": "rgb(31 200 228 / <alpha-value>)",
-          "300": "rgb(10 170 210 / <alpha-value>)",
-          "400": "rgb(13 148 189 / <alpha-value>)",
-          "500": "rgb(5 79 119 / <alpha-value>)",
-          "600": "rgb(18 27 59 / <alpha-value>)",
-        },
-        "redwave": {
-          "100": "rgb(255 25 44 / <alpha-value>)",
-          "200": "rgb(235 12 31 / <alpha-value>)",
-          "300": "rgb(191 7 22 / <alpha-value>)",
-          "400": "rgb(172 6 19 / <alpha-value>)",
-          "500": "rgb(133 5 16 / <alpha-value>)",
-        },
-        "cypurple": {
-          100: "rgb(250 169 122 / <alpha-value>)",
-          300: "rgb(116 12 68 / <alpha-value>)",
-          400: "rgb(56 9 14 / <alpha-value>)",
-          500: "rgb(32 20 16 / <alpha-value>)",
-        },
-        "grape": {
-          100: "rgb(195 48 178 / <alpha-value>)",
-          200: "rgb(115 17 108 / <alpha-value>)",
-          300: "rgb(100 30 176 / <alpha-value>)",
-          400: "rgb(30 6 45 / <alpha-value>)",
-        },
-        "lava": {
-          100: 'rgb(235 166 47 )',
-          200: "rgb(245 122 41 )",
-          300: "rgb(217 68 45 )",
-          400: 'rgb(191 54 33 )',
-          500: 'rgb(158 46 27 )'
-        }
-
-      },
       fontSize: {
 
         xxs: '0.5rem',
@@ -180,11 +107,6 @@ export default {
       gap: {
         "sm": 5.5,
       },
-
-
-      dropShadow: {
-      },
-
       padding: {
         "4.5": "1.125rem",
       },
@@ -236,44 +158,17 @@ export default {
         dialog: 771,
       },
 
-      neonGlowSize: {
-        xs: '0.015em',
-        sm: '0.03em',
-        md: '2px',
-        lg: '4px'
-      },
-
-
     },
   },
   plugins: [
-
-    plugin(({ matchUtilities, theme }) => {
-
-      matchUtilities({
-
-        'throb-on': (value) => ({
-          '--throb-on-color': value
-        }), 'throb-off': (value) => ({
-          '--throb-off-color': value
-        }),
-
-
-      }, {
-        respectPrefix: false,
-        values: flattenColorPalette(theme('colors')),
-        type: 'color',
-      });
-
-    }),
 
     plugin(({ addComponents, theme }) => {
 
       addComponents({
 
-        '.btn-sm': {
-          'min-width': '5em',
-          'max-width': '8em',
+        '.btn': {
+          'min-width': '8em',
+          'max-width': '16em',
           'min-height': '0.5em',
           'padding': `1px ${theme('padding.1')}`,
           '&:disabled': {
@@ -281,21 +176,7 @@ export default {
 
           }
         }
-      }),
-
-        addComponents({
-
-          '.btn': {
-            'min-width': '8em',
-            'max-width': '16em',
-            'min-height': '0.5em',
-            'padding': `1px ${theme('padding.1')}`,
-            '&:disabled': {
-              opacity: '0.5',
-
-            }
-          }
-        })
+      })
 
     }),
 
